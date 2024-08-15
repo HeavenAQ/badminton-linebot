8270b526bccc0c61039d2358fbb850b4bc650dcb
Heaven Chen <891118heaven@gmail.com>
2024-08-15 08:28:43 +0800

    [Deployment] Rewrite part of the code to deploy onto GCP with Google Cloud Run

---
+++ Dockerfile
@@ -0,0 +1,23 @@
+# Builder 
+FROM golang:latest AS builder
+WORKDIR /app
+
+# download the dependencies dependencies first so that they can be cached. 
+COPY go.mod go.sum ./
+RUN go mod download
+
+# copy the source code and build the go app
+COPY . .
+RUN go build -o main .
+
+# Runner
+FROM alpine:latest
+WORKDIR /root
+COPY --from=builder /app/main .
+
+# download the tool needed to run the app
+RUN apk update
+RUN apk add libc6-compat
+
+EXPOSE $PORT
+CMD ["/root/main"]
--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/HeavenAQ/app"
 	"github.com/joho/godotenv"
@@ -11,14 +12,15 @@ import (
 func main() {
 	// load env
 	if err := godotenv.Load(); err != nil {
-		log.Fatal(err)
+		log.Println("No .env file found")
+		log.Println("Trying to load from system environment variables")
 	}
 
 	app := app.NewApp()
 	http.HandleFunc("/callback", app.HandleCallback)
 
-	app.InfoLogger.Println("\n\tServer started on port 3000: http://localhost:3000")
-	if err := http.ListenAndServe(":"+"3000", nil); err != nil {
+	app.InfoLogger.Println("\n\tServer started on port: http://localhost:" + os.Getenv("PORT"))
+	if err := http.ListenAndServe(":"+os.Getenv("PORT"), nil); err != nil {
 		log.Fatal(err)
 	}
 }
